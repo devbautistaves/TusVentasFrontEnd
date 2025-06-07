e1fe72ebbf3acd7a743db3173900db602e028b22
Bautista <argenrgames@gmail.com>
2025-06-07 12:30:14 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,26 @@ import "./App.css"
 import Training from "./pages/Training"
 import AdminTraining from "./pages/AdminTraining"
 import Chat from "./pages/Chat"
+import { Helmet } from "react-helmet";
+
 
 function App() {
   return (
+      <>
+
+        <Helmet>
+        <meta name="keywords" content="Ventas, Comisiones, Emprender, Ingresos Extra, Independencia Financiera, Ser tu propio jefe, Comercializadora, Oportunidad Laboral, TusVentas, Bautista Vescio, Distribuidor, Trabajo Independiente" />
+        <meta name="description" content="Convertite en tu propio jefe vendiendo con nosotros. Ingresos por comisión, flexibilidad horaria y apoyo constante. Sumate a TusVentas y construí tu negocio hoy mismo." />
+        <meta name="author" content="BautistaVes" />
+        <meta name="copyright" content="BautistaVes" />
+        <meta name="robots" content="index" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tusventas.digital" />
+        <meta property="og:image" content="%PUBLIC_URL%/bannertusventas.png" />
+        <meta property="og:locale" content="es_AR" />
+        <meta property="og:site_name" content="TusVentasDigitales" />
+        <title>TusVentas - Sé tu propio jefe</title>
+      </Helmet>
     <AuthProvider>
       <Router>
         <Routes>
@@ -57,6 +74,7 @@ function App() {
         </Routes>
       </Router>
     </AuthProvider>
+    </>
   )
 }
 
